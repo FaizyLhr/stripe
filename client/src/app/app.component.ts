6945216444fb7fdb74b2f85b895033cfe3b91915
MuhammadFeezan <mfeezanlhr@gmail.com>
2022-03-07 17:23:48 +0500

    extra code removed

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { StripePaymentElementComponent, StripeService } from 'ngx-stripe';
-
-import { StripeElementsOptions } from '@stripe/stripe-js';
 
 import { PaymentService } from './core/services/payment.service';
 
@@ -13,19 +9,12 @@ import { PaymentService } from './core/services/payment.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  @ViewChild(StripePaymentElementComponent)
-  paymentElement!: StripePaymentElementComponent;
   stripeTest: any;
   serverStripe: any;
-  elementsOptions: StripeElementsOptions = { locale: 'en' };
-  paying!: boolean;
-  flag: boolean = true;
 
   constructor(
-    private http: HttpClient,
     private paymentService: PaymentService,
-    private fb: FormBuilder,
-    private stripeService: StripeService
+    private fb: FormBuilder
   ) {}
 
   ngOnInit() {
@@ -39,69 +28,6 @@ export class AppComponent implements OnInit {
       description: ['', [Validators.required]],
       amount: [, [Validators.required, Validators.pattern(/\d+/)]],
     });
-  }
-
-  pay() {
-    if (this.stripeTest.valid) {
-      this.paying = true;
-      this.stripeService
-        .confirmPayment({
-          elements: this.paymentElement?.elements,
-          confirmParams: {
-            payment_method_data: {
-              billing_details: {
-                name: this.stripeTest.get('name').value,
-              },
-            },
-          },
-          redirect: 'if_required',
-        })
-        .subscribe((result) => {
-          this.paying = false;
-          console.log('Result', result);
-          if (result.error) {
-            // Show error to your customer (e.g., insufficient funds)
-            console.log({ success: false, error: result.error.message });
-          } else {
-            console.log(result);
-            // The payment has been processed!
-            if (result?.paymentIntent?.status === 'succeeded') {
-              // Show a success message to your customer
-              console.log({ success: true });
-            }
-          }
-        });
-    } else {
-      console.log(this.stripeTest);
-    }
-  }
-
-  createPayment() {
-    this.paymentService.createPayment(this.stripeTest.value).subscribe(
-      (res: any) => {
-        console.log(res);
-        this.elementsOptions.clientSecret = res.client_secret;
-        this.flag = false;
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
-  }
-
-  checkout() {
-    this.paymentService.checkout().subscribe(
-      (res) => {
-        // if (res.error) {
-        //   alert(res.error.message);
-        // }
-        console.log(res);
-      },
-      (err) => {
-        console.log(err);
-      }
-      // Check the server.js tab to see an example implementation
-    );
   }
 
   // Server Side implementation
